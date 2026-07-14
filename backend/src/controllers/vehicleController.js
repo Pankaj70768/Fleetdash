a7d70c5823a5c7df66bf2bdeba4995b53bf9f502
Sarupuru Chakradhar <chakradharchowdarysarupuru@gmail.com>
2026-07-13 23:44:18 -0700

    Added GET APIs for Vehicle module

--- backend/src/controllers/vehicleController.js
+++ backend/src/controllers/vehicleController.js
@@ -19,7 +19,56 @@ const createVehicle = async (req, res) => {
 
   }
 };
+// Get all vehicles
 
+const getAllVehicles = async (req, res) => {
+
+    try {
+
+        const vehicles = await Vehicle.find();
+
+        res.status(200).json({
+            success: true,
+            count: vehicles.length,
+            data: vehicles
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+
+};
+// Get vehicle by ID
+const getVehicleById = async (req, res) => {
+    try {
+        const vehicle = await Vehicle.findById(req.params.id);
+
+        if (!vehicle) {
+            return res.status(404).json({
+                success: false,
+                message: "Vehicle not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: vehicle
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
 module.exports = {
-  createVehicle,
+    createVehicle,
+    getAllVehicles,
+    getVehicleById,
 };
