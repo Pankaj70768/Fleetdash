836a5f26a0d978792f4262f7599b17f6d19abb47
aditi <kopardeaditi935@gmail.com>
2026-07-21 19:38:36 +0530

    Frontend authentication integration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-
-const API = axios.create({
-
-    baseURL: "YOUR_BACKEND_URL"
-
+const api = axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
-
-export default API;
+export default api;
